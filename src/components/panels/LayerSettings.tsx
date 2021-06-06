eb8cfa76f78a22be1e5cd6c88a585e7f590fa0a8
Mat Sz <contact@matsz.dev>
2021-06-06 21:28:16 +0200

    replaced settings with react-dat-gui

--- src/components/panels/LayerSettings.tsx
+++ src/components/panels/LayerSettings.tsx
@@ -1,97 +1,22 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import Slider from 'rc-slider';
+import DatGui, {
+  DatNumber,
+  DatBoolean,
+  DatFolder,
+  DatColor,
+  DatSelect,
+} from 'react-dat-gui';
 
 import { useProjectStore } from '../../ProjectStore';
-import {
-  FilterLayer,
-  FilterSetting,
-  FilterSettingType,
-  LayerType,
-} from '../../types';
+import { FilterSettingType, LayerType } from '../../types';
 
-export const LayerSettingsSetting: React.FC<{
-  setting: FilterSetting;
-  layer: FilterLayer;
-}> = observer(({ setting, layer }) => {
-  const projectStore = useProjectStore();
-  const value = layer.settings[setting.key];
-
-  const renderSetting = () => {
-    switch (setting.type) {
-      case FilterSettingType.OFFSET:
-        return (
-          <>
-            <div className="input-wrapper">
-              <span>X</span>
-              <Slider
-                value={value[0]}
-                step={setting.step || 0.01}
-                min={-1}
-                max={1}
-                onChange={v => {
-                  value[0] = v;
-                  projectStore.requestPreviewRender();
-                }}
-              />
-            </div>
-            <div className="input-wrapper">
-              <span>Y</span>
-              <Slider
-                value={value[1]}
-                step={setting.step || 0.01}
-                min={-1}
-                max={1}
-                onChange={v => {
-                  value[1] = v;
-                  projectStore.requestPreviewRender();
-                }}
-              />
-            </div>
-          </>
-        );
-      case FilterSettingType.INTEGER:
-        return (
-          <>
-            <Slider
-              value={value}
-              step={setting.step || 1}
-              min={setting.minValue ?? -1}
-              max={setting.maxValue ?? 1}
-              onChange={v => {
-                layer.settings[setting.key] = v;
-                projectStore.requestPreviewRender();
-              }}
-            />
-          </>
-        );
-      case FilterSettingType.FLOAT:
-        return (
-          <>
-            <Slider
-              value={value}
-              step={setting.step || 0.01}
-              min={setting.minValue ?? -1}
-              max={setting.maxValue ?? 1}
-              onChange={v => {
-                layer.settings[setting.key] = v;
-                projectStore.requestPreviewRender();
-              }}
-            />
-          </>
-        );
-      default:
-        return null;
-    }
-  };
-
-  return (
-    <div className="layer-setting">
-      <strong>{setting.name}</strong>
-      {renderSetting()}
-    </div>
-  );
-});
+declare module 'react-dat-gui' {
+  interface DatSelectProps extends DatChangableFieldProps {
+    options: any[];
+    optionLabels: any[];
+  }
+}
 
 export const LayerSettings: React.FC = observer(() => {
   const projectStore = useProjectStore();
@@ -113,15 +38,107 @@ export const LayerSettings: React.FC = observer(() => {
     <div className="subpanel">
       <strong>Layer settings</strong>
       <div className="layer-settings">
-        {layer.type === LayerType.IMAGE || !layer.filter?.settings
-          ? 'This layer has no settings.'
-          : layer.filter.settings.map(setting => (
-              <LayerSettingsSetting
-                key={setting.key}
-                setting={setting}
-                layer={layer}
-              />
-            ))}
+        {layer.type === LayerType.IMAGE || !layer.filter?.settings ? (
+          'This layer has no settings.'
+        ) : (
+          <DatGui
+            data={layer.settings}
+            onUpdate={data => {
+              for (const setting of layer.filter.settings!) {
+                if (setting.type === FilterSettingType.SELECT) {
+                  data[setting.key] = parseInt(data[setting.key]);
+                }
+              }
+
+              layer.settings = data;
+              projectStore.requestPreviewRender();
+            }}
+          >
+            {layer.filter.settings.map(setting => {
+              if (!setting.key) {
+                return null;
+              }
+
+              const name = setting.name || setting.key;
+
+              switch (setting.type) {
+                case FilterSettingType.INTEGER:
+                  return (
+                    <DatNumber
+                      min={setting.minValue ?? -1}
+                      max={setting.maxValue ?? 1}
+                      step={setting.step ?? 1}
+                      path={setting.key}
+                      label={name}
+                      key={setting.id}
+                    />
+                  );
+                case FilterSettingType.FLOAT:
+                  return (
+                    <DatNumber
+                      min={setting.minValue ?? -1}
+                      max={setting.maxValue ?? 1}
+                      step={setting.step ?? 0.01}
+                      path={setting.key}
+                      label={name}
+                      key={setting.id}
+                    />
+                  );
+                case FilterSettingType.COLOR:
+                  return (
+                    <DatColor
+                      path={setting.key}
+                      label={name}
+                      key={setting.id}
+                    />
+                  );
+                case FilterSettingType.BOOLEAN:
+                  return (
+                    <DatBoolean
+                      path={setting.key}
+                      label={name}
+                      key={setting.id}
+                    />
+                  );
+                case FilterSettingType.OFFSET:
+                  return (
+                    <DatFolder title={name} closed={false} key={setting.id}>
+                      <DatNumber
+                        min={-1}
+                        max={1}
+                        step={0.01}
+                        path={setting.key + '.0'}
+                        label={'X'}
+                      />
+                      <DatNumber
+                        min={-1}
+                        max={1}
+                        step={0.01}
+                        path={setting.key + '.1'}
+                        label={'Y'}
+                      />
+                    </DatFolder>
+                  );
+                case FilterSettingType.SELECT:
+                  return (
+                    <DatSelect
+                      path={setting.key}
+                      label={name}
+                      key={setting.id}
+                      options={
+                        setting.selectValues?.map(value => value.value) || []
+                      }
+                      optionLabels={
+                        setting.selectValues?.map(value => value.name) || []
+                      }
+                    />
+                  );
+              }
+
+              return null;
+            })}
+          </DatGui>
+        )}
       </div>
     </div>
   );
