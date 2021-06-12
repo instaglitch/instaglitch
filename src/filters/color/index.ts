397d19e8a9b60f57f4c29dd1f293cfa6522320e0
Mat Sz <contact@matsz.dev>
2021-06-12 18:23:19 +0200

    added RGB offset (clip)

--- src/filters/color/index.ts
+++ src/filters/color/index.ts
@@ -1,3 +1,4 @@
 import rgb_offset from './rgb_offset.json';
+import rgb_offset_clip from './rgb_offset_clip.json';
 
-export const filtersColor = [rgb_offset];
+export const filtersColor = [rgb_offset, rgb_offset_clip];
