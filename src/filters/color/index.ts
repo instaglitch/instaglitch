760da61a55e35869335b72ac4220094a7dceff8a
Mat Sz <contact@matsz.dev>
2021-06-15 19:43:13 +0200

    added a colorize filter

--- src/filters/color/index.ts
+++ src/filters/color/index.ts
@@ -1,4 +1,5 @@
 import rgb_offset from './rgb_offset.json';
 import rgb_offset_clip from './rgb_offset_clip.json';
+import colorize from './colorize.json';
 
-export const filtersColor = [rgb_offset, rgb_offset_clip];
+export const filtersColor = [rgb_offset, rgb_offset_clip, colorize];
