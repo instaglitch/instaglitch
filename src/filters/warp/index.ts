164b526662e0ec5ff27a5de53b44901fae7cc18f
Mat Sz <contact@matsz.dev>
2021-06-15 20:05:32 +0200

    added mirrored split filters

--- src/filters/warp/index.ts
+++ src/filters/warp/index.ts
@@ -1,5 +1,13 @@
 import tile from './tile.json';
 import tile_mirrored from './tile_mirrored.json';
 import kaleidoscope from './kaleidoscope.json';
+import mirrored_split_horizontal from './mirrored_split_horizontal.json';
+import mirrored_split_vertical from './mirrored_split_vertical.json';
 
-export const filtersWarp = [tile, tile_mirrored, kaleidoscope];
+export const filtersWarp = [
+  tile,
+  tile_mirrored,
+  kaleidoscope,
+  mirrored_split_horizontal,
+  mirrored_split_vertical,
+];
