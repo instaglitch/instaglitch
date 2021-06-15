c96538265e6b72624d402c8722f55777ba30ed04
Mat Sz <contact@matsz.dev>
2021-06-15 20:16:10 +0200

    added mirror filters

--- src/filters/warp/index.ts
+++ src/filters/warp/index.ts
@@ -1,6 +1,8 @@
 import tile from './tile.json';
 import tile_mirrored from './tile_mirrored.json';
 import kaleidoscope from './kaleidoscope.json';
+import mirror_horizontal from './mirror_horizontal.json';
+import mirror_vertical from './mirror_vertical.json';
 import mirrored_split_horizontal from './mirrored_split_horizontal.json';
 import mirrored_split_vertical from './mirrored_split_vertical.json';
 
@@ -8,6 +10,8 @@ export const filtersWarp = [
   tile,
   tile_mirrored,
   kaleidoscope,
+  mirror_horizontal,
+  mirror_vertical,
   mirrored_split_horizontal,
   mirrored_split_vertical,
 ];
