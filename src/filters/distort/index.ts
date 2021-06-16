0689a301a3aad1db86b921d50e022376f0cd1bff
Mat Sz <contact@matsz.dev>
2021-06-16 20:11:40 +0200

    added Pixelate filters

--- src/filters/distort/index.ts
+++ src/filters/distort/index.ts
@@ -1,5 +1,13 @@
 import fisheye from './fisheye.json';
 import wave from './wave.json';
 import fractal_julia from './fractal_julia.json';
+import pixelate from './pixelate.json';
+import pixelate_polar from './pixelate_polar.json';
 
-export const filtersDistort = [fisheye, wave, fractal_julia];
+export const filtersDistort = [
+  fisheye,
+  wave,
+  fractal_julia,
+  pixelate,
+  pixelate_polar,
+];
