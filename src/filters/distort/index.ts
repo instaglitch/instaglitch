d6adf09ecdc0a59108ceeb7173928f329dd5e78c
Mat Sz <contact@matsz.dev>
2021-06-16 18:26:22 +0200

    added a Fractal (Julia) filter

--- src/filters/distort/index.ts
+++ src/filters/distort/index.ts
@@ -1,4 +1,5 @@
 import fisheye from './fisheye.json';
 import wave from './wave.json';
+import fractal_julia from './fractal_julia.json';
 
-export const filtersDistort = [fisheye, wave];
+export const filtersDistort = [fisheye, wave, fractal_julia];
