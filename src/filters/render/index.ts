fbf8f96db39e5d5dad9e014bacd447f9a855187e
Mat Sz <contact@matsz.dev>
2021-06-16 21:00:34 +0200

    added a Linocut filter

--- src/filters/render/index.ts
+++ src/filters/render/index.ts
@@ -1,4 +1,5 @@
 import vignette from './vignette.json';
 import scanlines from './scanlines.json';
+import linocut from './linocut.json';
 
-export const filtersRender = [vignette, scanlines];
+export const filtersRender = [vignette, scanlines, linocut];
