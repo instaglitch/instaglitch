76a561057b63b2695c5da2239cc4e997e7bba1ed
Mat Sz <contact@matsz.dev>
2021-06-16 20:39:12 +0200

    added duotone and posterize

--- src/filters/color/index.ts
+++ src/filters/color/index.ts
@@ -1,5 +1,13 @@
 import rgb_offset from './rgb_offset.json';
 import rgb_offset_clip from './rgb_offset_clip.json';
 import colorize from './colorize.json';
+import duotone from './duotone.json';
+import posterize from './posterize.json';
 
-export const filtersColor = [rgb_offset, rgb_offset_clip, colorize];
+export const filtersColor = [
+  rgb_offset,
+  rgb_offset_clip,
+  colorize,
+  duotone,
+  posterize,
+];
