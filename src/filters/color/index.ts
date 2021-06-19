438f910ed01dd3b5ee610d39557347951e998475
Mat Sz <contact@matsz.dev>
2021-06-19 16:11:22 +0200

    added a HSL offset filter

--- src/filters/color/index.ts
+++ src/filters/color/index.ts
@@ -4,6 +4,8 @@ import hsl from './hsl.json';
 import hsl_multiply from './hsl_multiply.json';
 import rgb_offset from './rgb_offset.json';
 import rgb_offset_clip from './rgb_offset_clip.json';
+import hsl_offset from './hsl_offset.json';
+import hsl_offset_clip from './hsl_offset_clip.json';
 import colorize from './colorize.json';
 import duotone from './duotone.json';
 import posterize from './posterize.json';
@@ -15,6 +17,8 @@ export const filtersColor = [
   hsl_multiply,
   rgb_offset,
   rgb_offset_clip,
+  hsl_offset,
+  hsl_offset_clip,
   colorize,
   duotone,
   posterize,
