283a6a63a755874646865d4879150f21d569ba32
Mat Sz <contact@matsz.dev>
2021-06-19 16:04:22 +0200

    added HSL and RGB effects

--- src/filters/color/index.ts
+++ src/filters/color/index.ts
@@ -1,3 +1,7 @@
+import rgb from './rgb.json';
+import rgb_multiply from './rgb_multiply.json';
+import hsl from './hsl.json';
+import hsl_multiply from './hsl_multiply.json';
 import rgb_offset from './rgb_offset.json';
 import rgb_offset_clip from './rgb_offset_clip.json';
 import colorize from './colorize.json';
@@ -5,6 +9,10 @@ import duotone from './duotone.json';
 import posterize from './posterize.json';
 
 export const filtersColor = [
+  rgb,
+  rgb_multiply,
+  hsl,
+  hsl_multiply,
   rgb_offset,
   rgb_offset_clip,
   colorize,
