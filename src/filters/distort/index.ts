38989453e6001ffec96e14855d84ce45eeb3e01d
Mat Sz <contact@matsz.dev>
2021-06-24 18:40:36 +0200

    added gausian and radial blurs

--- src/filters/distort/index.ts
+++ src/filters/distort/index.ts
@@ -3,6 +3,8 @@ import wave from './wave.json';
 import fractal_julia from './fractal_julia.json';
 import pixelate from './pixelate.json';
 import pixelate_polar from './pixelate_polar.json';
+import radial_blur from './radial_blur.json';
+import gaussian_blur from './gaussian_blur.json';
 
 export const filtersDistort = [
   fisheye,
@@ -10,4 +12,6 @@ export const filtersDistort = [
   fractal_julia,
   pixelate,
   pixelate_polar,
+  radial_blur,
+  gaussian_blur,
 ];
