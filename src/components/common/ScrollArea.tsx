471b29302386ece876d5c607ec939f43ed336f2d
Mat Sz <contact@matsz.dev>
2021-06-28 18:39:01 +0200

    added ScrollArea bar drag

--- src/components/common/ScrollArea.tsx
+++ src/components/common/ScrollArea.tsx
@@ -35,11 +35,108 @@ export const ScrollArea: React.FC<ScrollAreaProps> = ({
       recalculateBarX();
     };
 
+    let dragging: 'x' | 'y' | undefined = undefined;
+    let initX = 0;
+    let initY = 0;
+    let initScrollTop = 0;
+    let initScrollLeft = 0;
+
+    const onStartDrag = (bar: 'x' | 'y' | undefined, x: number, y: number) => {
+      dragging = bar;
+      initX = x;
+      initY = y;
+      initScrollTop = content.scrollTop;
+      initScrollLeft = content.scrollLeft;
+
+      if (bar === 'x') {
+        barX.classList.add('scrollarea-bar-active');
+      }
+    };
+
+    const onMoveDrag = (x: number, y: number) => {
+      if (!dragging) return;
+
+      if (dragging === 'x') {
+        const areaRect = area.getBoundingClientRect();
+        const diff = y - initY;
+        content.scrollTop =
+          initScrollTop + (diff / areaRect.height) * content.scrollHeight;
+      }
+    };
+
+    const onEndDrag = () => {
+      dragging = undefined;
+
+      barX.classList.remove('scrollarea-bar-active');
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!dragging) return;
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      onMoveDrag(e.clientX, e.clientY);
+    };
+
+    const handleTouchMove = (e: TouchEvent) => {
+      if (!dragging) return;
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      const touch = e.touches[0];
+      if (!touch) {
+        return;
+      }
+
+      onMoveDrag(touch.clientX, touch.clientY);
+    };
+
+    const onMouseDownX = (e: MouseEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      onStartDrag('x', e.clientX, e.clientY);
+    };
+
+    const onTouchStartX = (e: TouchEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const touch = e.touches[0];
+      if (!touch) {
+        return;
+      }
+
+      onStartDrag('x', touch.clientX, touch.clientY);
+    };
+
     onScroll();
     content.addEventListener('scroll', onScroll);
 
+    barX.addEventListener('mousedown', onMouseDownX);
+    barX.addEventListener('touchstart', onTouchStartX);
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', onEndDrag);
+
+    document.addEventListener('touchmove', handleTouchMove);
+    document.addEventListener('touchend', onEndDrag);
+    document.addEventListener('touchcancel', onEndDrag);
+
     return () => {
       content.removeEventListener('scroll', onScroll);
+
+      barX.removeEventListener('mousedown', onMouseDownX);
+      barX.removeEventListener('touchstart', onTouchStartX);
+
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', onEndDrag);
+
+      document.removeEventListener('touchmove', handleTouchMove);
+      document.removeEventListener('touchend', onEndDrag);
+      document.removeEventListener('touchcancel', onEndDrag);
     };
   });
 
