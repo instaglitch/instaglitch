360d0a38368d39947b8de7e945991a58feca3761
Mat Sz <contact@matsz.dev>
2023-04-10 13:21:23 +0200

    switch to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import { configure } from 'mobx';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,11 +8,12 @@ configure({
   enforceActions: 'never',
 });
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
